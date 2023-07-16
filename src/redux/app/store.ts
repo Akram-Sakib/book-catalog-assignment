980a3f0a69119781d21bc44f94df24e2d82a2503
akram-sakib <sayedakramsakib@gmail.com>
2023-07-16 09:17:40 +0600

    completed initial redux setup

---
+++ src/redux/app/store.ts
@@ -0,0 +1,16 @@
+import { configureStore } from "@reduxjs/toolkit";
+import { apiSlice } from "../features/api/apiSlice";
+
+export const store = configureStore({
+  reducer: {
+    [apiSlice.reducerPath]: apiSlice.reducer,
+  },
+  devTools: process.env.NODE_ENV !== "production",
+  middleware: (getDefaultMiddlewares) =>
+    getDefaultMiddlewares({
+      serializableCheck: false,
+    }).concat(apiSlice.middleware),
+});
+
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
---
+++ src/redux/features/api/apiSlice.ts
@@ -0,0 +1,18 @@
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+// import { RootState } from "../../app/store";
+
+export const apiSlice = createApi({
+  reducerPath: "api",
+  baseQuery: fetchBaseQuery({
+    baseUrl: "http://localhost:5000/api/v1/",
+    credentials: "include",
+    // prepareHeaders: (headers, { getState }) => {
+    //   const token = (getState() as RootState)?.auth?.accessToken;
+    //   if (token) {
+    //     headers.set("Authorization", `${token}`);
+    //   }
+    //   return headers;
+    // },
+  }),
+  endpoints: () => ({}),
+});
